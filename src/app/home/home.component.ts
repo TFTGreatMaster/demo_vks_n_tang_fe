8b3023f53dff38fce5440758a046267399924352
Ngo Van Chung <chungnv.eledevo@gmail.com>
2024-05-20 13:02:13 +0700

    tich hop

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,7 +31,7 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {
   }
 
-  private DOMAIN = 'http://192.168.1.44:8082'
+  private DOMAIN = 'http://192.168.1.50:8082'
 
   breadCrumb: {
     items: MenuItem[],
@@ -100,7 +100,7 @@ export class HomeComponent implements OnInit {
       });
   }
 
-  handleReload(){
+  handleReload() {
     const idParent = this.breadCrumb.items[this.breadCrumb.items.length - 1].id
     const nameParent: string = this.breadCrumb.items[this.breadCrumb.items.length - 1].label!
     this.handleGetDocuments({id: Number(idParent), name: nameParent})
@@ -116,23 +116,24 @@ export class HomeComponent implements OnInit {
   }
 
   onCreateConfirm(): void {
+    console.log('this.folderCreate.name', this.folderCreate.name)
     this.isCreateModal = false;
     const idParent = this.breadCrumb.items[this.breadCrumb.items.length - 1].id
-    console.log('idParent', idParent)
-    // this.http.post(`${this.DOMAIN}/document/${idParent}`, {
-    //   file: this.folderCreate
-    // }).subscribe((res: any) => {
-    // this.handleReload()
-    // })
+    this.http.post(`${this.DOMAIN}/vu-an/1/documents`, {
+      name: this.folderCreate.name,
+      type: this.folderCreate.type,
+      parent_document_id: idParent
+    }).subscribe(() => {
+      this.handleReload()
+    })
   }
 
   onUpdateItem(data: ISelectItem) {
-    // this.http.post(`${this.DOMAIN}/document/${data.id}`, {
-    //   name: data.name,
-    //   type: data.type
-    // }).subscribe((res: any) => {
-    //  this.handleReload()
-    // })
+    this.http.put(`${this.DOMAIN}/document/update/${data.id}`, {
+      name: data.name,
+    }).subscribe(() => {
+      this.handleReload()
+    })
   }
 
   onCancelModal(): void {
@@ -140,17 +141,17 @@ export class HomeComponent implements OnInit {
   }
 
   onUpload(event: any): void {
+    console.log('event', event)
     const idParent = this.breadCrumb.items[this.breadCrumb.items.length - 1].id
-    console.log('idParent', idParent)
-    console.log("🚀 ~ HomeComponent ~ onUpload ~ event:", event)
-    // this.http.post(`${this.DOMAIN}/document/upload/${idParent}`, {
-    //   file: event.files
-    // }).subscribe((res: any) => {
-    // this.handleReload()
-    // })
+    const formData = new FormData();
+    formData.append(`file`, event.files[0]);
+    this.http.post(`${this.DOMAIN}/document/create-file/${idParent}`, formData).subscribe(() => {
+      this.handleReload()
+    })
   }
 
   onDeleteItem(id: number) {
+    console.log('idid', id)
     this.http.delete(`${this.DOMAIN}/document/${id}`).subscribe(() => {
       this.handleReload()
     })
